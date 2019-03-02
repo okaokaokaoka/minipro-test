35c3ed98d7cb2208654db1340d8db6e7a437c4fc
岡田徹 <toruhandbell0424@gmail.com>
2019-03-02 20:43:01 +0900

    add text message

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,7 @@ const app = getApp()
 Page({
   data: {
     motto: 'うんこわーるど',
+    hoge: 'これはテストなんだよオラオラ',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
@@ -24,7 +25,7 @@ Page({
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+      app.userInfoReadyCallback  = res => {
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
